805d04a8622c00664e5926db201a075b5bb1b224
Krippakrull <krippakrull@gmail.com>
2022-04-22 20:24:49 +0000

    add Tailwind and DaisUI, tested working

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -26,6 +26,17 @@
 		try editing <strong>src/routes/index.svelte</strong>
 	</h2>
 
+	<div class="card w-96 bg-base-100 shadow-xl">
+		<figure><img src="./svelte-welcome.png" alt="Shoes" /></figure>
+		<div class="card-body">
+		  <h2 class="card-title">Shoes!</h2>
+		  <p>If a dog chews shoes whose shoes does he choose?</p>
+		  <div class="card-actions justify-end">
+			<button class="btn btn-primary">Buy Now</button>
+		  </div>
+		</div>
+	  </div>
+
 	<Counter />
 </section>
 
--- svelte.config.js
+++ svelte.config.js
@@ -4,7 +4,11 @@ import preprocess from 'svelte-preprocess';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
@@ -13,7 +17,9 @@ const config = {
 			server: {
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443 : 24678,
-					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
+					host: process.env.HMR_HOST
+						? process.env.HMR_HOST.substring('https://'.length)
+						: 'localhost'
 				}
 			}
 		}
